b16aa71d51877bb0e54c989bc651bab068a4da24
纳特琴 <32509481+NutterChen@users.noreply.github.com>
2020-08-23 18:20:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (strpos($_SERVER['HTTP_USER_AGENT'], 'Alipay') !== false) {
     $userid = $_GET['userid'];
     $amount = $_GET['amount'];
     $memo = $_GET['memo'];
-	$html = file_get_contents(__DIR__ . "/template.php");
+    $html = file_get_contents(__DIR__ . "/template.php");
     $html = str_replace('itsuserid', $userid, $html);
     $html = str_replace('itsamount', $amount, $html);
     $html = str_replace('itsmemo', $memo,  $html);
